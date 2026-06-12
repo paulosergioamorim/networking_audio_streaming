caf0e96271298139cbbcc3a0d8d697f5c47207fd
Paulo Sérgio <pamorimmonico@gmail.com>
2026-06-12 07:30:22 -0300

    feat: aff LOG_ERRNO macros

--- src/logger.h
+++ src/logger.h
@@ -41,6 +41,9 @@ extern "C" {
 #define LOG_INFO(fmt, ...)  logger_log(LogLevel_INFO , __FILENAME__, __LINE__, fmt, ##__VA_ARGS__)
 #define LOG_WARN(fmt, ...)  logger_log(LogLevel_WARN , __FILENAME__, __LINE__, fmt, ##__VA_ARGS__)
 #define LOG_ERROR(fmt, ...) logger_log(LogLevel_ERROR, __FILENAME__, __LINE__, fmt, ##__VA_ARGS__)
+#define LOG_ERRNO() logger_log(LogLevel_ERROR, __FILENAME__, __LINE__, "error (%d) %s", errno, strerror(errno))
+#define LOG_CUSTOM_ERRNO(fmt, ...)                                                                                     \
+    logger_log(LogLevel_ERROR, __FILENAME__, __LINE__, "error (%d) %s: " fmt, errno, strerror(errno), ##__VA_ARGS__)
 #define LOG_FATAL(fmt, ...) logger_log(LogLevel_FATAL, __FILENAME__, __LINE__, fmt, ##__VA_ARGS__)
 
 typedef enum {
